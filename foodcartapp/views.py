eba88e3b19475408138749f523947542bcdd96ee
Fdrv <oldgot@gmail.com>
2022-05-23 20:21:00 +0300

    Rewrite data validation logic with DRF

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,7 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
-import phonenumbers
+from rest_framework.serializers import ModelSerializer
 
 from django.http import JsonResponse
 from django.templatetags.static import static
@@ -61,72 +60,37 @@ def product_list_api(request):
     })
 
 
+class OrderProductSerializer(ModelSerializer):
+    class Meta:
+        model = OrderProduct
+        fields = ['product', 'quantity']
+
+
+class OrderSerializer(ModelSerializer):
+    products = OrderProductSerializer(many=True, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = ['firstname', 'lastname', 'phonenumber', 'address', 'products']
+
+
 @api_view(['POST'])
 def register_order(request):
-    order_raw = request.data
-    print(order_raw)
-    try:
-        assert isinstance(order_raw['products'], list)
-    except KeyError:
-        return Response(
-            {'error': 'products: Обязательное поле'},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-    except AssertionError:
-        return Response(
-            {'error': 'products: Ожидался list со значениями'},
-            status=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        )
-    try:
-        if not all([order_raw['firstname'], order_raw['lastname'], order_raw['phonenumber'], order_raw['address']]):
-            return Response(
-                {'error': 'firstname, lastname, phonenumber, address: Это поле не может быть пустым'},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-    except KeyError:
-        return Response(
-            {'error': 'firstname, lastname, phonenumber, address: Обязательное поле'},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-    if not isinstance(order_raw['firstname'], str):
-        return Response(
-            {'error': 'firstname: Not a valid string.'},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-    try:
-        phonenumber = phonenumbers.parse(order_raw['phonenumber'])
-    except phonenumbers.NumberParseException as err:
-        return Response(
-            {'error': err},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-    if not phonenumbers.is_valid_number(phonenumber):
-        return Response(
-            {'error': 'phonenumber: Введен некорректный номер телефона'},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-    if order_raw.get('products'):
-        order = Order.objects.create(firstname=order_raw['firstname'],
-                                     lastname=order_raw['lastname'],
-                                     phonenumber=order_raw['phonenumber'],
-                                     address=order_raw['address'],
-                                     )
-        for order_item in order_raw['products']:
-            try:
-                product = Product.objects.get(pk=order_item['product'])
-            except Product.DoesNotExist:
-                return Response(
-                    {'error': f'products: Недопустимый первичный ключ {order_item["product"]}'}
-                )
-            OrderProduct(product=product,
-                         quantity=order_item['quantity'],
-                         order=order,
-                         ).save()
-        return Response(
-            {'status': 'OK'},
-            status=status.HTTP_200_OK,
-        )
+    serializer = OrderSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    validated_data = serializer.validated_data
+
+    order = Order.objects.create(
+        firstname=validated_data['firstname'],
+        lastname=validated_data['lastname'],
+        phonenumber=validated_data['phonenumber'],
+        address=validated_data['address'],
+    )
+
+    ordered_products_fields = validated_data['products']
+    ordered_products = [OrderProduct(order=order, **fields) for fields in ordered_products_fields]
+    OrderProduct.objects.bulk_create(ordered_products)
+
     return Response(
-        {'error': 'products: Этот список не может быть пустым'},
-        status=status.HTTP_400_BAD_REQUEST,
+        {'order_id': order.id},
     )
