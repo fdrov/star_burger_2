b1c70da791efa3144d58dc7e14e0e71c21543b66
server <oldgot@gmail.com>
2024-02-06 10:04:10 +0000

    fix DATABASES initialization

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -86,15 +86,11 @@ MEDIA_URL = '/media/'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=env.dj_db_url(
-            'DATABASE_URL',
-            default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3')),
-        ),
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3')),
         conn_max_age=600,
         conn_health_checks=True,
     )
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
